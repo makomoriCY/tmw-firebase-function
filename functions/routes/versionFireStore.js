d21877340c00bd193be0dc47e5d8e5c9f8621fd8
makomoriCY <cachayootpong.bao@innosoft.kmutt.ac.th>
2021-08-30 15:33:04 +0700

    create function get by id

--- functions/routes/versionFireStore.js
+++ functions/routes/versionFireStore.js
@@ -8,17 +8,32 @@ const firestore = firebase.firestore()
 
 const versionFireStore = express()
 
-versionFireStore.post('/', async (req, res) => {
+// versionFireStore.post('/', async (req, res) => {
+//   try {
+//     const data = req.body
+//     await firestore
+//       .collection('transaction')
+//       .doc()
+//       .set(data)
+//     res.send('Record saved successfuly')
+//   } catch (error) {
+//       console.log(`msg : ${error} `)
+//     // res.status(400).send(error.message)
+//   }
+// })
+
+versionFireStore.get('/:id', async (req, res) => {
   try {
-    const data = req.body
-    await firestore
-      .collection('transaction')
-      .doc()
-      .set(data)
-    res.send('Record saved successfuly')
+    const id = req.params.id
+    const transactionId = await firestore.collection('transaction').doc(id)
+    const data = await transactionId.get()
+    if (!data.exists) {
+      res.status(404).send('transaction  not found')
+    } else {
+      res.send(data.data())
+    }
   } catch (error) {
-      console.log(`msg : ${error} `)
-    // res.status(400).send(error.message)
+    console.log(`msg : ${error} `)
   }
 })
 
