684cca03fc7ee9e5fe9552d228276a668478dec6
makomoriCY <cachayootpong.bao@innosoft.kmutt.ac.th>
2021-08-30 15:09:55 +0700

    test function add

--- functions/routes/versionFireStore.js
+++ functions/routes/versionFireStore.js
@@ -2,10 +2,24 @@ const functions = require('firebase-functions')
 const builderFunction = functions.region('us-central1').https
 const express = require('express')
 
+const firebase = require('../db')
+const Transaction = require('../models/transaction')
+const firestore = firebase.firestore()
+
 const versionFireStore = express()
 
-versionFireStore.get('/', async (req, res) => {
-  
+versionFireStore.post('/', async (req, res) => {
+  try {
+    const data = req.body
+    await firestore
+      .collection('transaction')
+      .doc()
+      .set(data)
+    res.send('Record saved successfuly')
+  } catch (error) {
+      console.log(`msg : ${error} `)
+    // res.status(400).send(error.message)
+  }
 })
 
 exports.versionFireStore = builderFunction.onRequest(versionFireStore)
