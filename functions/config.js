86a9ef47d100767a053483335c17a823a47c9730
makomoriCY <cachayootpong.bao@innosoft.kmutt.ac.th>
2021-09-01 17:33:05 +0700

    add db url

--- functions/config.js
+++ functions/config.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 const {
   API_KEY,
   AUTH_DOMAIN,
-  // DATABASE_URL,
+  DATABASE_URL,
   PROJECT_ID,
   STORAGE_BUCKET,
   MESSAGING_SENDER_ID,
@@ -14,7 +14,7 @@ module.exports = {
   firebaseConfig: {
     apiKey: API_KEY,
     authDomain: AUTH_DOMAIN,
-    // databaseURL: DATABASE_URL,
+    databaseURL: DATABASE_URL,
     projectId: PROJECT_ID,
     storageBucket: STORAGE_BUCKET,
     messagingSenderId: MESSAGING_SENDER_ID,
