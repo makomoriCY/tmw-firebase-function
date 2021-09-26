ee58a94952cf9d094a51ed65ea13131da976dccf
makomoriCY <chayootpong.bao@innosoft.kmutt.ac.th>
2021-09-27 03:34:27 +0700

    improve handler error

--- functions/routes/getBlockList.js
+++ functions/routes/getBlockList.js
@@ -10,9 +10,9 @@ getBlockList.get('/', async (req, res) => {
   try {
     const { userId } = req.body
 
-    const userProfile = await getProfileFromAmity(userId)
+    const list = (await getProfileFromAmity(userId)) || []
 
-    return res.send(userProfile || [])
+    return res.send(list)
   } catch (error) {
     console.log(`error in getBlockList function: ${error}`)
     res.status(500)
@@ -29,13 +29,33 @@ getBlockList.get('/', async (req, res) => {
         `${process.env.PROD_URL}/v3/users/${id}`,
         configAuth
       )
-
+      console.log(profileAmity)
       return profileAmity.data.users[0].metadata.blockList
     } catch (error) {
       console.log(`ERRORs in getAmityProfile id: ${id} : ${error}`)
-      return res.status(500).send('Server Errors')
+      return res
+        .status(ConvertStatusCode(error.response.data.code))
+        .send(error.response.data.message)
     }
   }
 })
+
+function ConvertStatusCode (status) {
+  let code
+  switch (status) {
+    case 400400:
+      code = 404
+      break
+    case 400300:
+      code = 403
+      break
+    case 500000:
+      code = 500
+      break
+    default:
+      code = 500
+  }
+  return code
+}
 
 exports.getBlockList = builderFunction.onRequest(getBlockList)
