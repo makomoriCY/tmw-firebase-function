26d561b52191a609ca466e559012a03def01ede6
makomoriCY <chayootpong.bao@innosoft.kmutt.ac.th>
2021-11-12 17:37:04 +0700

    edit function send noti

--- functions/routes/sendNotification.js
+++ functions/routes/sendNotification.js
@@ -1,6 +1,8 @@
 const functions = require('firebase-functions')
 const builderFunction = functions.region('asia-southeast1').https
 const express = require('express')
+const axios = require('axios')
+require('dotenv').config()
 
 const sendNotification = express()
 
@@ -8,36 +10,56 @@ sendNotification.post('/', async (req, res) => {
   try {
     const {
       event,
-      data: { messages }
+      data: { messages, users }
     } = req.body
 
     const { userId: senderId, channelId } = messages[0]
 
-    console.log({ senderId, channelId })
+    const amount = users[0]?.metadata?.amount
 
     if (event !== 'message.didCreate') {
       console.log('reject event' + event)
       return res.status(422).send('Event Incorrect')
     }
 
-    // check user profile with true backend
-    const senderProfile = await getProfileFromTrue(sender)
-    const receiverProfile = await getProfileFromTrue(receiver)
+    const userData = await getUserFromChanel(channelId)
 
-    // get sender block list
-    const senderBlockList = await getProfileFromAmity(senderProfile)
+    const receiverProfile = userData?.filter(user => user.userId !== senderId)
 
-    // get receiver block list
-    const receiverBlockList = await getProfileFromAmity(receiverProfile)
+    const receiverId = receiverProfile[0].userId
 
-    // find sender user blocked
+    const senderBlockList = users[0]?.metadata?.blockList
+
+    const receiverBlockList = receiverProfile[0]?.metadata?.blockList
+
     const isSenderBlockReceiver = senderBlockList?.find(user => {
-      return user === receiverProfile?.userId
+      return user === receiverId
     })
 
-    //find receiver user blocked
     const isReceiverBlockSender = receiverBlockList?.find(user => {
-      return user === senderProfile?.userId
+      return user === senderId
+    })
+
+    const response = {
+      templateName: 'p2p',
+      tmnId: `tmn.${receiverId}`,
+      properties: {
+        from: senderId.toString(),
+        amount: amount.toString()
+      }
+    }
+
+    console.log({
+      senderId,
+      channelId,
+      amount,
+      userData,
+      receiverProfile,
+      senderBlockList,
+      receiverBlockList,
+      isSenderBlockReceiver,
+      isReceiverBlockSender,
+      response
     })
 
     // ถ้า status 500 ให้ retry 3 ครั้ง
@@ -45,32 +67,22 @@ sendNotification.post('/', async (req, res) => {
 
     if (!isSenderBlockReceiver) {
       !isReceiverBlockSender
-        ? res.send(
-            pushNotification({ id: receiverProfile?.userId, msg: messages })
-          )
-        : res.send(
-            dropNotification({ id: receiverProfile?.userId, msg: messages })
-          )
+        ? res.send(pushNotification(response))
+        : res.send(dropNotification())
     } else {
-      res.send(dropNotification({ id: receiverProfile?.userId, msg: messages }))
+      res.send(dropNotification())
     }
   } catch (error) {
     console.log(`ERRORs in sendNotification function: ${error}`)
-    console.log('Message: ', req.body?.message?.id)
-    console.log('Sender: ', req.body?.senderProfile?.userId)
-    console.log('Receiver: ', req.body?.receiverProfile?.userId)
   }
 })
 
-function pushNotification ({ id, msg }) {
-  const status = {
-    status: true
-  }
-  console.log(`Notification status : ${status.status}`)
-  return status
+function pushNotification (response) {
+  const data = response
+  return data
 }
 
-function dropNotification ({ id, msg }) {
+function dropNotification () { 
   const status = {
     status: false
   }
@@ -78,14 +90,23 @@ function dropNotification ({ id, msg }) {
   return status
 }
 
-function getProfileFromTrue (profile) {
-  return profile
-}
+async function getUserFromChanel (channelId) {
+  const token =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjoiZmRmZWFhMGY2Yzk5YTUyNjE0NTNkMjQ3MDc1NjA0ODJjMjVkODRlN2IyNjEzYTI4Y2JiYWNmMTgwNmVhMzExNTI1YTEwMzAzNjM5YmNjODYxMDA1NDMwYzE0YzQ5Yzk3NTgzNDcxYjdmNDQzMzUwYzc3MjE3YTkwMjE4ZWEzM2YzODBiMjAzNGFhNWMwYTVmZmRjMWM4Yjg5MzAxYTYxZDNiOGUzMzMzYjAxZTgwYzAwNGQ1M2ZiMTZhNGJhZDEzN2JhMjJkZTAxMWE5MDE3ZjhhYjFlMTZmNTYyM2QwNzNlZTM1MmUwMWMyYzdlNjAzZmM1OWI2NWU3Mjk5NjkwYWIzNzEyOTMzNjMxZTk1IiwiaWF0IjoxNjM2NjkyMzMxLCJleHAiOjE2NjgyMjgzMzF9.xP5lXEExR46HfGq9FbWq6Tn_ef0T14TTpEpJHvxhQAo'
+  const configAuth = {
+    headers: { Authorization: `Bearer ${token}` }
+  }
 
-// check profile & get block list from amity backend
-// ใช้แบบจำลองเพราะข้อมูลของจริงยังไม่มี blocklist
-function getProfileFromAmity (profile) {
-  return profile?.metadata?.blockList
+  try {
+    const { data } = await axios.get(
+      `${process.env.PROD_URL}/v3/channels/${channelId}/users`,
+      configAuth
+    )
+    const users = data.users
+    return users
+  } catch (error) {
+    console.log(`getUserFromChanel() msg : ${error}`)
+  }
 }
 
 exports.sendNotification = builderFunction.onRequest(sendNotification)
